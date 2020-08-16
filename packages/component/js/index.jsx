f972ee585eb0d0e106245f71bf0f302c1a5c8ae0
Daniel Maddison <dmaddison@icloud.com>
2020-08-16 11:37:11 +1000

    Added packages/component

---
+++ packages/component/js/index.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { greet } from '@your-scope/core';
+// import './css/style.scss';
+
+// const Component = ({ name }) => <p>{greet(name)}</p>;
+
+// Component.defaultProps = {
+//   name: '',
+// };
+
+// Component.propTypes = {
+//   name: PropTypes.string,
+// };
+
+// export default Component;
